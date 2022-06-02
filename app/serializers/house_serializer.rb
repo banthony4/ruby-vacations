7c568c13b96db3299f78b8cdeca20beb3748e352
Rebecca Rozmajzl <rebecca.rozmajzl@gmail.com>
2022-06-02 15:48:19 -0400

    fix house serializer

--- app/serializers/house_serializer.rb
+++ app/serializers/house_serializer.rb
@@ -2,7 +2,7 @@ class HouseSerializer < ActiveModel::Serializer
   attributes :id, :name, :per_night, :location, :description, :image, :avg_rating
   
   def avg_rating
-    object.reviews.sum(:rating) / object.reviews.size
+    object.reviews.size > 0 ? (object.reviews.sum(:rating) / object.reviews.size) : nil
   end
 
 end
